7a789e804ab179529573594423e821f8c4fbf135
Sam Bowering <samjbowering@gmail.com>
2021-06-25 21:48:01 +1200

    Fixing form event handler

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -4,18 +4,20 @@ import { Form } from 'semantic-ui-react'
 class Question extends React.Component {
   // vvv Placeholder for backend vvv
   state = {
-    answer: '',
+    answer: null,
     numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   }
   // ^^^ Placeholder for backend ^^^
 
-  
+  handleChange = (e) => {
+    this.setState({ answer: e.value })
+  }
 
   render () {
     return (
       <div className='question-container'>
         <p>What is {this.state.numbers[8]} x {this.state.numbers[4]}?</p>
-        <Form.Input placeholder='Go Shnooks! <3' name='answer' value={this.state.answer} style={{ position: 'relative', top: '1vh', left: '1vw', height: '3vh', width: '5vw' }}></Form.Input>
+        <Form.Input onChange={this.handleChange} value={this.state.answer} placeholder='Go Shnooks! <3' name='answer' style={{ position: 'relative', top: '1vh', left: '1vw', height: '3vh', width: '5vw' }}></Form.Input>
       </div>
     )
   }
